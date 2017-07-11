2f6212127c5840748f225649b6d695e90d2812ca
Sumeet Shahani <sumeet.shahani@ves.ac.in>
2017-07-11 16:56:46 +0530

    adding index and css file for admin-home section

---
+++ admin_php/index.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sumeet
+ * Date: 11-07-2017
+ * Time: 16:35
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Section</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../assests/bootstrap/css/bootstrap.min.css">
+    <!-- bootstrap theme-->
+    <link rel="stylesheet" href="../assests/bootstrap/css/bootstrap-theme.min.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="../assests/font-awesome/css/font-awesome.min.css">
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="../custom/css/admin-home.css">
+
+    <!-- DataTables -->
+    <link rel="stylesheet" href="../assests/plugins/datatables/jquery.dataTables.min.css">
+
+    <!-- file input -->
+    <link rel="stylesheet" href="../assests/plugins/fileinput/css/fileinput.min.css">
+
+    <!-- jquery -->
+    <script src="../assests/jquery/jquery.min.js"></script>
+    <!-- jquery ui -->
+    <link rel="stylesheet" href="../assests/jquery-ui/jquery-ui.min.css">
+    <script src="../assests/jquery-ui/jquery-ui.min.js"></script>
+
+    <!-- bootstrap js -->
+    <script src="../assests/bootstrap/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+    
+</body>
+</html>
+
+
